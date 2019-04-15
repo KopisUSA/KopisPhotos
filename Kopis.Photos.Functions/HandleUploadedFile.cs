f0feae9bd304f2c16efe7e86b2c297d0b8f48bcf
Christopher Smith <chris.smith@kopisusa.com>
2019-04-15 02:29:24 -0400

    Fixed issue with storage connection name Added AppOffline to Function deployment

--- Kopis.Photos.Functions/HandleUploadedFile.cs
+++ Kopis.Photos.Functions/HandleUploadedFile.cs
@@ -8,7 +8,7 @@ namespace Kopis.Photos.Functions
     public static class HandleUploadedFile
     {
         [FunctionName("HandleUploadedFile")]
-        public static void Run([BlobTrigger("uploaded/{name}", Connection = "UploadedStorage")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("uploaded/{name}", Connection = "UploadStorage")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
