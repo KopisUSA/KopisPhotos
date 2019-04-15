d256b4d38e934a8f497f789c449b49bf135c8fe5
Christopher Smith <chris.smith@kopisusa.com>
2019-04-15 02:22:37 -0400

    Added Functions project and deployment

---
+++ Kopis.Photos.Functions/HandleUploadedFile.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Kopis.Photos.Functions
+{
+    public static class HandleUploadedFile
+    {
+        [FunctionName("HandleUploadedFile")]
+        public static void Run([BlobTrigger("uploaded/{name}", Connection = "UploadedStorage")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
